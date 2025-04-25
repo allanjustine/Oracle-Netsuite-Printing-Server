5a97a1273c1625c1fa30a693463e3d2cd624d387
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-04-25 17:16:44 +0800

    admin side functions added

--- app/Http/Controllers/Api/V1/ReceiptController.php
+++ app/Http/Controllers/Api/V1/ReceiptController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Events\ReceiptRecords;
 use App\Http\Controllers\Controller;
 use App\Models\Receipt;
 use Illuminate\Http\Request;
@@ -11,13 +12,54 @@ class ReceiptController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $receipts = Receipt::all();
+        // fetch all receipts
+        $searchTerm = $request->query('search');
+        $receipts = Receipt::when($searchTerm, function ($query) use ($searchTerm) {
+            $query->where("print_by", "LIKE", "%{$searchTerm}%");
+        })
+            ->paginate(20);
+
+        // fetch latest 10 receipts
+        $latest_receipts = Receipt::latest()->take(10)->get();
+
+        // get the total count of today and yesterday receipts
+        $todays_receipts_count = Receipt::whereToday('created_at')->count();
+        $yesterdays_receipts_count = Receipt::whereBeforeToday('created_at')->count();
+
+        // get the total weekly and last week receipts counts
+
+        $weekly_receipts_count = Receipt::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->count();
+        $last_weekly_receipts_count = Receipt::whereBetween('created_at', [now()->subWeek()->startOfWeek(), now()->subWeek()->endOfWeek()])->count();
+
+        // get the total count of monthly and last month receipts
+        $monthly_receipts_count = Receipt::whereMonth('created_at', now())->count();
+        $last_monthly_receipts_count = Receipt::whereMonth('created_at', now()->subMonth()->month)->count();
+
+        // get the total count of total branch who have printed records
+        $total_branch_print_records = Receipt::distinct('print_by')->count();
+
+        // calculating percentage of todays receipts
+        $todays_percentage = $yesterdays_receipts_count > 0 ? number_format($todays_receipts_count / $yesterdays_receipts_count * 100, 2) : number_format(100, 2);
+
+        // calculating percentage of weekly receipts
+        $weekly_percentage = $last_weekly_receipts_count > 0 ? number_format($weekly_receipts_count / $last_weekly_receipts_count * 100, 2) : number_format(100, 2);
+
+        // calculating percentage of monthly receipts
+        $monthly_percentage = $last_monthly_receipts_count > 0 ? number_format($monthly_receipts_count / $last_monthly_receipts_count * 100, 2) : number_format(100, 2);
 
         return response()->json([
-            'message'       => "All receipts fetched successfully",
-            'receipts'      => $receipts
+            'message'                      => "All receipts fetched successfully",
+            'receipts'                     => $receipts,
+            'latest_receipts'              => $latest_receipts,
+            'todays_receipts_count'        => $todays_receipts_count,
+            'weekly_receipts_count'        => $weekly_receipts_count,
+            'monthly_receipts_count'       => $monthly_receipts_count,
+            'total_branch_print_records'   => $total_branch_print_records,
+            'todays_percentage'            => $todays_percentage,
+            'monthly_percentage'           => $monthly_percentage,
+            'weekly_percentage'            => $weekly_percentage,
         ], 200);
     }
 
@@ -55,6 +97,8 @@ class ReceiptController extends Controller
             'print_by'          => $request->print_by,
             'print_count'       => 1
         ]);
+
+        ReceiptRecords::dispatch($receipt);
 
         return response()->json([
             'message'       => ucfirst($request->external_id) . " receipt is created successfully.",
