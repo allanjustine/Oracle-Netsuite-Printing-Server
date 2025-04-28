f8a1d90a2ba901e1356a80ccc089e842b7c29eb4
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-04-28 16:06:45 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,19 +15,26 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://netsuite-print.smctgroup.ph', 'http://localhost:3000', 'https://netsuite-server.smctgroup.ph'],
+    'allowed_origins' => [
+        'https://netsuite-print.smctgroup.ph',
+        'https://netsuite-server.smctgroup.ph',
+        'http://localhost:3000'
+    ],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => [
+        'Authorization',
+        'X-Socket-ID'
+    ],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => true,
 
