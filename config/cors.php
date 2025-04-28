ad13ec6a96a776b2ade0d24eb4328ce82efa76a1
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-04-28 16:01:57 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://netsuite-print.smctgroup.ph', 'http://localhost:3000'],
+    'allowed_origins' => ['https://netsuite-print.smctgroup.ph', 'http://localhost:3000', 'https://netsuite-server.smctgroup.ph'],
 
     'allowed_origins_patterns' => [],
 
