e4c261e9b9fff0a73008046270d963181f980d09
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-09-20 10:34:58 +0800

    added redirect on printing

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::redirect('/', 'https://printing.smctgroup.ph');
+Route::redirect('/login', 'https://printing.smctgroup.ph');
+Route::redirect('/dashboard', 'https://printing.smctgroup.ph');
